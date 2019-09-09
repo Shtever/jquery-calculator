cbbea04a159f9b7d32a2ddb55283222251f3bde0
Stephen McMillen <Shtevetm@gmail.com>
2019-09-09 13:38:02 -0500

    first number pressed displays in console and in result card

---
+++ java.js
@@ -0,0 +1,16 @@
+// set function for  +,-,*,/ -globl
+
+// set variable for x, y
+
+// set variable for result
+
+
+
+
+//listen
+$(".number").on("click",function(){
+    console.log($(this).val());
+var firstNumber = $("#first-number")
+console.log(firstNumber);
+firstNumber.text($(this).val());
+})
